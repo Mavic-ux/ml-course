c2bca8394afc4228b0c979a4c27f86b0ad8a48e6
Aznaur <aliev.ak@phystech.edu>
2023-05-15 22:57:32 +0300

    added 2 assignment

--- homeworks_advanced/assignment02_three_headed_network/network.py
+++ homeworks_advanced/assignment02_three_headed_network/network.py
@@ -13,13 +13,19 @@ class ThreeInputsNet(nn.Module):
     def __init__(self, n_tokens, n_cat_features, concat_number_of_features, hid_size=64):
         super(ThreeInputsNet, self).__init__()
         self.title_emb = nn.Embedding(n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>        
-        
-        self.full_emb = nn.Embedding(num_embeddings=n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>
-        
-        self.category_out = # <YOUR CODE HERE>
+        self.title_conv = nn.Conv1d(in_channels=hid_size, out_channels=2*hid_size, kernel_size=3)
+        self.title_act = nn.ReLU()
+        self.title_avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.title_out = nn.Flatten()
 
+        self.full_emb = nn.Embedding(num_embeddings=n_tokens, embedding_dim=hid_size)
+        self.full_conv = nn.Conv1d(in_channels=hid_size, out_channels=2*hid_size, kernel_size=3)
+        self.full_act = nn.ReLU()
+        self.title_avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.full_out = nn.Flatten()
+        
+        self.category_linear = nn.Linear(in_features=n_cat_features, out_features=hid_size*2)
+        self.category_out = nn.ReLU()
 
         # Example for the final layers (after the concatenation)
         self.inter_dense = nn.Linear(in_features=concat_number_of_features, out_features=hid_size*2)
@@ -30,12 +36,19 @@ class ThreeInputsNet(nn.Module):
     def forward(self, whole_input):
         input1, input2, input3 = whole_input
         title_beg = self.title_emb(input1).permute((0, 2, 1))
-        title = # <YOUR CODE HERE>
+        title = self.title_conv(title_beg)
+        title = self.title_act(title)
+        title = self.title_avg_pool(title)
+        title = self.title_out(title)
         
         full_beg = self.full_emb(input2).permute((0, 2, 1))
-        full = # <YOUR CODE HERE>        
+        full = self.full_conv(full_beg)
+        full = self.full_act(full)
+        full = self.title_avg_pool(full)
+        full = self.full_out(full)      
         
-        category = # <YOUR CODE HERE>        
+        category = self.category_linear(input3)    
+        category = self.category_out(category)
         
         concatenated = torch.cat(
             [
@@ -45,6 +58,8 @@ class ThreeInputsNet(nn.Module):
             ],
             dim=1)
         
-        out = # <YOUR CODE HERE>
+        out = self.inter_dense(concatenated)
+        out = self.full_act(out)
+        out = self.final_dense(out)
         
         return out
